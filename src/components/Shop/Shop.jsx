a960f70869e47de467d09e3fa34bbac8ccc1925c
Atish Dipankar Chakraborty <atishanindya@gmail.com>
2023-03-28 21:50:01 +0600

    cart completed

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
@@ -27,7 +28,8 @@ const Shop = () => {
              }
             </div>
             <div className='cart-container'>
-                <h6>Selected Item: {cart.length}</h6>
+                <Cart cart={cart}></Cart>
+               
             </div>
             
         </div>
