fc38d511d26495a3755cb5f9cbf889c40193a9dd
Atish Dipankar Chakraborty <atishanindya@gmail.com>
2023-04-07 14:16:51 +0600

    Orders delete saved in local storage

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
+import "./Orders.css";
 import { useLoaderData } from 'react-router-dom';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import { removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
-    const cart=useLoaderData();
-    console.log(cart)
+    const savedCart = useLoaderData();
+    const [cart, setCart] = useState(savedCart)
+
+    const handleRemoveFromCart = (id) => {
+        console.log(id)
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+
+    }
     return (
         <div className='shop-container'>
-            <div className='products-container'>
-                <h2>Order Page: {cart.length}</h2>
+            <div className='review-container'>
+                {
+                    cart.map(product => <ReviewItem
+                        key={product.id}
+                        product={product}
+                        handleRemoveFromCart={handleRemoveFromCart}
+                    ></ReviewItem>)
+                }
             </div>
             <div className='cart-container'>
                 <Cart cart={cart}></Cart>
